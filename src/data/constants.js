ff72ec11ef1b50e1ff7a32da9cb073310463d358
vanshchauhan21 <143607775+vanshchauhan21@users.noreply.github.com>
2024-12-22 20:55:43 +0530

    #udate

--- src/data/constants.js
+++ src/data/constants.js
@@ -400,7 +400,7 @@ export const projects = [
     tags: ["javascript"," html5","css"],
     category: "web app",
     github: "hhttps://github.com/vanshchauhan21/Shoes-Store-.git",
-    webapp: "https://shoes-store-virid-six.vercel.app/",
+    webapp: "https://shoes-store-virid-six.vercel.app",
     
   },
   {
@@ -421,7 +421,7 @@ export const projects = [
     title: "Color-Picker-extension",
     date: "march 2024 - April 2024",
     description:
-      "The ultimate color picker extension brings creativity and efficiency on your web browsing experience. empowers you to effortlessly identify, capture, and utilize colors from any webpage with precision and ease. ",
+      "The ultimate color picker extension  creativity and efficiency on your web browsing experience. empowers you to effortlessly identify, capture, and utilize colors from any webpage with precision and ease. ",
     image:
       "https://github.com/vanshchauhan21/Color-Picker-extension/blob/main/color.png?raw=true",
     tags: ["javascript"," html5","css"],
